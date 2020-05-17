8a1b579713e15c9c2845d68ecec538315fb177cb
高树平 <1411198314@qq.com>
2020-05-17 10:44:09 +0800

    1.新增url为空判断 2.新增加载默认图片方法

--- androidutils/src/main/java/com/satsna/utils/glide/GlideUtil.java
+++ androidutils/src/main/java/com/satsna/utils/glide/GlideUtil.java
@@ -6,6 +6,7 @@ import android.widget.ImageView;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 import com.satsna.utils.R;
+import com.satsna.utils.utils.StringUtil;
 
 /**
  * Glide图片框架加载及加载失败配置
@@ -14,15 +15,14 @@ public class GlideUtil {
     //这里面配置Glide的默认图片,前提是用Glide加载的时候用了这里的
 
     //默认加载图片
-    public static int placeholder= R.mipmap.iv_default_loading;//加载中
-    public static int error=R.mipmap.iv_default_fail;//加载失败
+    public static int placeholder = R.mipmap.iv_default_loading;//加载中
+    public static int error = R.mipmap.iv_default_fail;//加载失败
     public static int fallback = R.mipmap.iv_default_loading;
 
     public static RequestOptions requestOptions = new RequestOptions()
             .placeholder(placeholder)
             .error(error)
             .fallback(fallback);
-
 
 
     /**
@@ -38,19 +38,40 @@ public class GlideUtil {
         GlideUtil.fallback = fallback;
     }
 
+    public static void load(Context context, ImageView imageView) {
+        Glide.with(context).load(error).into(imageView);
+    }
+
+    public static void load(Context context, Integer imageRes, ImageView imageView) {
+        if (imageRes == null) {
+            Glide.with(context).load(error).into(imageView);
+        } else {
+            Glide.with(context).load(imageRes).into(imageView);
+        }
+
+    }
+
     public static void load(Context context, String url, ImageView imageView) {
+        if (StringUtil.isEmpty(url)) {
+            load(context, imageView);
+            return;
+        }
         Glide.with(context).load(url).apply(getApply()).into(imageView);
     }
 
     /**
      * 加载图片
      *
-     * @param context     上下文
-     * @param url         图片路径
+     * @param context    上下文
+     * @param url        图片路径
      * @param defaultImg 默认图片 placeHolder error fallBack 均为defaultImg
-     * @param imageView   显示的ImageView控件
+     * @param imageView  显示的ImageView控件
      */
     public static void load(Context context, String url, Integer defaultImg, ImageView imageView) {
+        if (StringUtil.isEmpty(url)) {
+            load(context, imageView);
+            return;
+        }
         Glide.with(context).load(url).apply(getApply(defaultImg)).into(imageView);
     }
 
@@ -64,6 +85,10 @@ public class GlideUtil {
      * @param imageView   显示的ImageView控件
      */
     public static void load(Context context, String url, Integer placeHolder, Integer error, ImageView imageView) {
+        if (StringUtil.isEmpty(url)) {
+            load(context, imageView);
+            return;
+        }
         Glide.with(context).load(url).apply(getApply(placeHolder, error)).into(imageView);
     }
 
@@ -78,6 +103,10 @@ public class GlideUtil {
      * @param imageView   显示的ImageView控件
      */
     public static void load(Context context, String url, Integer placeHolder, Integer error, Integer fallBack, ImageView imageView) {
+        if (StringUtil.isEmpty(url)) {
+            load(context, imageView);
+            return;
+        }
         Glide.with(context).load(url).apply(getApply(placeHolder, error, fallBack)).into(imageView);
     }
 
@@ -88,6 +117,12 @@ public class GlideUtil {
      * @return 默认配置RequestOptions
      */
     public static RequestOptions getApply() {
+        if (requestOptions == null) {
+            requestOptions = new RequestOptions()
+                    .placeholder(placeholder)
+                    .error(error)
+                    .fallback(fallback);
+        }
         return requestOptions;
     }
 
