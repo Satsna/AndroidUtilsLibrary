9240cd24ee333da8168a32fe1b31e596e000d705
高树平 <1411198314@qq.com>
2020-09-23 21:06:47 +0800

    新增设置Toast显示位置

--- androidutils/src/main/java/com/satsna/utils/utils/ToastUtil.java
+++ androidutils/src/main/java/com/satsna/utils/utils/ToastUtil.java
@@ -1,19 +1,33 @@
 package com.satsna.utils.utils;
 
 import android.content.Context;
+import android.view.Gravity;
 import android.widget.Toast;
 
 public class ToastUtil {
     public static Toast toast = null;
+    private static int gravity = Gravity.CENTER_HORIZONTAL | Gravity.BOTTOM;
+
+    /**
+     * 设置吐司位置
+     *
+     * @param gravity 位置
+     */
+    public static void setGravity(final int gravity) {
+        ToastUtil.gravity = gravity;
+    }
+
 
     /**
      * 显示长Toast
+     *
      * @param context
      * @param text
      */
     public static void longshow(Context context, String text) {
         if (toast == null) {
             toast = Toast.makeText(context.getApplicationContext(), text, Toast.LENGTH_LONG);
+            toast.setGravity(gravity, 0, 0);
         } else {
             toast.setText(text);
         }
@@ -22,12 +36,14 @@ public class ToastUtil {
 
     /**
      * 显示长Toast
+     *
      * @param context
      * @param resId
      */
     public static void longshow(Context context, int resId) {
         if (toast == null) {
             toast = Toast.makeText(context.getApplicationContext(), resId, Toast.LENGTH_LONG);
+            toast.setGravity(gravity, 0, 0);
         } else {
             toast.setText(resId);
         }
@@ -36,26 +52,32 @@ public class ToastUtil {
 
     /**
      * 显示短Toast
+     *
      * @param context
      * @param text
      */
     public static void shortshow(Context context, String text) {
         if (toast == null) {
             toast = Toast.makeText(context.getApplicationContext(), text, Toast.LENGTH_SHORT);
+            toast.setGravity(gravity, 0, 0);
         } else {
             toast.setText(text);
         }
+
+
         toast.show();
     }
 
     /**
      * 显示短Toast
+     *
      * @param context
      * @param resId
      */
     public static void shortshow(Context context, int resId) {
         if (toast == null) {
             toast = Toast.makeText(context.getApplicationContext(), resId, Toast.LENGTH_SHORT);
+            toast.setGravity(gravity, 0, 0);
         } else {
             toast.setText(resId);
         }
--- example/src/main/java/com/example/stasna/MainActivity.java
+++ example/src/main/java/com/example/stasna/MainActivity.java
@@ -3,48 +3,25 @@ package com.example.stasna;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
-import android.os.Environment;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.view.Gravity;
+import android.view.View;
 
-import com.blankj.utilcode.util.FileUtils;
-import com.satsna.utils.glide.GlideUtil;
-import com.satsna.utils.utils.ImageUtils;
 import com.satsna.utils.utils.ToastUtil;
 
 public class MainActivity extends Activity {
     public String tag = getClass().getSimpleName();
     public Context context = this;
 
-    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ToastUtil.setGravity(Gravity.CENTER);
 
-        ImageView iv = findViewById(R.id.iv);
-        tv = findViewById(R.id.tv);
+    }
 
-        String url = "http://i0.hdslb.com/bfs/archive/839b48daee6bed189d49aa6eac912b353ce0db3d.jpg";
-        //获取图片扩展名
-        String extension = "." + FileUtils.getFileExtension(url);
-        String imagePath = Environment.getExternalStorageDirectory() + "/1test/图片/" + System.currentTimeMillis() + extension;
-        ImageUtils.SaveImageFromNetListener listener = new ImageUtils.SaveImageFromNetListener() {
-            @Override
-            public void onSuccess(String urlStr, String savePath) {
-                ToastUtil.shortshow(context, "保存成功");
-                tv.setText("保存成功");
-            }
-
-            @Override
-            public void onError(Exception e) {
-                ToastUtil.shortshow(context, "保存失败");
-                tv.setText("保存失败=" + e.getMessage());
-
-            }
-        };
-        ImageUtils.saveFromNet(this, url, imagePath, listener);
-
+    public void test(View v) {
+        ToastUtil.shortshow(context, "吐司");
     }
 }
