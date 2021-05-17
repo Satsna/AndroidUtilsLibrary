d0b7c94ff667cbc48720485c92e7714e9aea8aa8
高树平 <1411198314@qq.com>
2021-05-17 23:50:45 +0800

    解决sdk 29兼容性问题 新增星期工具类

---
+++ androidutils/src/main/java/com/satsna/utils/utils/WeekUtils.java
@@ -0,0 +1,30 @@
+package com.satsna.utils.utils;
+
+import java.util.Calendar;
+
+public class WeekUtils {
+
+    /*获取星期几*/
+    public static String getWeek() {
+        Calendar cal = Calendar.getInstance();
+        int i = cal.get(Calendar.DAY_OF_WEEK);
+        switch (i) {
+            case 1:
+                return "星期日";
+            case 2:
+                return "星期一";
+            case 3:
+                return "星期二";
+            case 4:
+                return "星期三";
+            case 5:
+                return "星期四";
+            case 6:
+                return "星期五";
+            case 7:
+                return "星期六";
+            default:
+                return "";
+        }
+    }
+}
